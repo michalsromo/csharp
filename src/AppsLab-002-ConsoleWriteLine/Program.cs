bea8f36d3c4d10412e5a221f16c114133405053a
Miško Šromovský <miskosromo@gmail.com>
2025-09-10 11:41:20 +0200

    dal som meno a vek

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int age = 10;
+string name = "george";
+
+Console.WriteLine(age);
+Console.WriteLine(name);
+
+Console.WriteLine($"My name is {name} and I am {age} years old.");
