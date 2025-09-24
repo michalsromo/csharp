0542a33ac14ce68dcac694cc7de1fcfbdf36ace2
miskosromo <misko@LAPTOP-QDNVIMJ4>
2025-09-24 13:01:55 +0200

    uloha8

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,11 @@
 ﻿Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
-Console.WriteLine("Ahoj, " + meno);
+Console.WriteLine(" Ahoj, " + meno + " koľko máš rokov? ");
 
-Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
-Console.WriteLine("Máš " + vek + " rokov.");
+Console.WriteLine("Aha,máš teda " + vek + " rokov.");
+
+Console.WriteLine("Aké je tvoje obľúbené jedlo? ");
+string jedlo = Console.ReadLine() ?? "0";
+Console.WriteLine(jedlo + " to znie skvele. ");
