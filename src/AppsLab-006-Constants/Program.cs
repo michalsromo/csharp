7fdd866b94a47f051401b22e85ac0e250a93c971
miskosromo <misko@LAPTOP-QDNVIMJ4>
2025-09-24 10:35:43 +0200

    uloha6

--- src/AppsLab-006-Constants/Program.cs
+++ src/AppsLab-006-Constants/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int MaxPoints = 200;
+const int MinAgeForDriversLicense = 16;
+const string WelcomeMessage = "Welcome to the Gulag!";
+
+Console.WriteLine(MaxPoints);
+Console.WriteLine(MinAgeForDriversLicense);
+Console.WriteLine(WelcomeMessage);
+
